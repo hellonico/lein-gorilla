073831bf57104b39d8ec15c5cfa1ebc04d474c54
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-31 20:26:29 +0900

    Version 0.4.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hellonico/lein-gorilla "0.4.1"
+(defproject hellonico/lein-gorilla "0.4.2-SNAPSHOT"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
