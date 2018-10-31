a61d50da2e4c1e237a1b71fcba5eeb745d6b163c
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-31 20:25:25 +0900

    change name and deps

--- project.clj
+++ project.clj
@@ -1,6 +1,18 @@
-(defproject lein-gorilla "0.4.0"
+(defproject hellonico/lein-gorilla "0.4.1-SNAPSHOT"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.4.0"]]
+  :dependencies [[hellonico/gorilla-repl "0.4.1"]]
+  :repositories [
+    ["vendredi" {:url "https://repository.hellonico.info/repository/hellonico/" :creds :gpg}]
+  ]
+  :release-tasks [["vcs" "assert-committed"]
+                ["change" "version" "leiningen.release/bump-version" "release"]
+                ["vcs" "commit"]
+                ["vcs" "tag" "--no-sign"]
+                ["deploy" "vendredi"]
+                ["change" "version" "leiningen.release/bump-version"]
+                ["vcs" "commit"]
+                ["vcs" "push"]
+                ]
   :eval-in-leiningen true)
